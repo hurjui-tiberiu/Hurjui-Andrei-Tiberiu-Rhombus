482efb20209e7099410865908bcd81368631f166
Hurjui Andrei <atiberiu88@gmail.com>
2022-11-05 23:23:50 +0200

    feature/implement-camera-movement

---
+++ Hurjui Andrei-Tiberiu-Rhombus/Cube.cs
@@ -0,0 +1,135 @@
+﻿using AdministratorFisierNS;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Rhombus
+{
+    public class Cube
+    {
+        public List<Color> colors = new List<Color>();
+        private readonly List<List<float>> coordonate = new List<List<float>>();
+        Admin admin = new Admin();
+     
+
+        public Cube()
+        {
+            coordonate = admin.GetCoordonate();
+
+            SetColors();
+        }
+
+        public void transparency(int a)
+        {
+           
+
+        }
+
+        public void SetColors()
+        {
+            colors.Clear();
+            Random rnd = new Random();
+            //Gemerarea a 24 de culori random pentru fiecare vertex
+            for (int i = 0; i < 24; i++)
+                colors.Add(Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256)));
+
+
+
+            for (int i = 0; i < colors.Count; i++)
+            {
+
+                Console.WriteLine("Vertex[" + i + "]:" + colors[i].ToString() + ", " + colors[i].ToString() + ", " + colors[i].ToString());
+            }
+
+            Console.WriteLine("\n");
+        }
+
+
+        //Desenarea cubului
+        public void DrawCube()
+        {
+
+             GL.Clear(ClearBufferMask.ColorBufferBit);
+          
+           
+
+            GL.Begin(PrimitiveType.Polygon);
+
+
+            GL.Color3(colors[0]);
+            GL.Vertex3(coordonate[0][0], coordonate[0][1], coordonate[0][2]);
+            GL.Color3(colors[1]);
+            GL.Vertex3(coordonate[1][0], coordonate[1][1], coordonate[1][2]);
+            GL.Color3(colors[2]);
+            GL.Vertex3(coordonate[2][0], coordonate[2][1], coordonate[2][2]);
+            GL.Color3(colors[3]);
+            GL.Vertex3(coordonate[3][0], coordonate[3][1], coordonate[3][2]);
+            GL.End();
+            
+
+            GL.Begin(PrimitiveType.Polygon);
+            GL.Color3(colors[4]);
+            GL.Vertex3(coordonate[4][0], coordonate[4][1], coordonate[4][2]);
+            GL.Color3(colors[5]);
+            GL.Vertex3(coordonate[5][0], coordonate[5][1], coordonate[5][2]);
+            GL.Color3(colors[6]);
+            GL.Vertex3(coordonate[6][0], coordonate[6][1], coordonate[6][2]);
+            GL.Color3(colors[7]);
+            GL.Vertex3(coordonate[7][0], coordonate[7][1], coordonate[7][2]);
+            GL.End();
+
+
+            GL.Begin(PrimitiveType.Polygon);
+            GL.Color3(colors[8]);
+            GL.Vertex3(coordonate[8][0], coordonate[8][1], coordonate[8][2]);
+            GL.Color3(colors[9]);
+            GL.Vertex3(coordonate[9][0], coordonate[9][1], coordonate[9][2]);
+            GL.Color3(colors[10]);
+            GL.Vertex3(coordonate[10][0], coordonate[10][1], coordonate[10][2]);
+            GL.Color3(colors[11]);
+            GL.Vertex3(coordonate[11][0], coordonate[11][1], coordonate[11][2]);
+            GL.End();
+
+
+            GL.Begin(PrimitiveType.Polygon);
+            GL.Color3(colors[12]);
+            GL.Vertex3(coordonate[12][0], coordonate[12][1], coordonate[12][2]);
+            GL.Color3(colors[13]);
+            GL.Vertex3(coordonate[13][0], coordonate[13][1], coordonate[13][2]);
+            GL.Color3(colors[14]);
+            GL.Vertex3(coordonate[14][0], coordonate[14][1], coordonate[14][2]);
+            GL.Color3(colors[15]);
+            GL.Vertex3(coordonate[15][0], coordonate[15][1], coordonate[15][2]);
+            GL.End();
+
+
+            GL.Begin(PrimitiveType.Polygon);
+            GL.Color3(colors[16]);
+            GL.Vertex3(coordonate[16][0], coordonate[16][1], coordonate[16][2]);
+            GL.Color3(colors[17]);
+            GL.Vertex3(coordonate[17][0], coordonate[17][1], coordonate[17][2]);
+            GL.Color3(colors[18]);
+            GL.Vertex3(coordonate[18][0], coordonate[18][1], coordonate[18][2]);
+            GL.Color3(colors[19]);
+            GL.Vertex3(coordonate[19][0], coordonate[19][1], coordonate[19][2]);
+            GL.End();
+
+
+
+            GL.Begin(PrimitiveType.Polygon);
+
+            GL.Color3(colors[23]);
+            GL.Vertex3(coordonate[20][0], coordonate[20][1], coordonate[20][2]);
+            GL.Color3(colors[20]);
+            GL.Vertex3(coordonate[21][0], coordonate[21][1], coordonate[21][2]);
+            GL.Color3(colors[21]);
+            GL.Vertex3(coordonate[22][0], coordonate[22][1], coordonate[22][2]);
+            GL.Color3(colors[22]);
+            GL.Vertex3(coordonate[23][0], coordonate[23][1], coordonate[23][2]);
+
+            GL.End();
+
+        }
+    }
+}
--- Hurjui Andrei-Tiberiu-Rhombus/Program.cs
+++ Hurjui Andrei-Tiberiu-Rhombus/Program.cs
@@ -1,124 +1,212 @@
-﻿using OpenTK;
+﻿
+using OpenTK;
+using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
 using System;
 using System.Drawing;
 
-namespace OpenTK_console_sample01
+namespace Rhombus
 {
-    class SimpleWindow : GameWindow
+    internal class Window3D : GameWindow
     {
-        private float rotationAngle = 0.0f;
+        bool showCube = true;
+        KeyboardState lastKeyPress;
+        private int transStep = 0;
+        private int radStep = 0;
+        private int attStep = 0;
+        Cube cube = new Cube();
+        Vector3 posEye = new Vector3(0, 0, 30);
+        private Camera _camera;
 
-        public SimpleWindow() : base(800, 600)
-        {
-            KeyDown += Keyboard_KeyDown;
-            Console.WriteLine("W-move up");
-            Console.WriteLine("S-move down");
-            Console.WriteLine("A-move left");
-            Console.WriteLine("D-move right");
-            Console.WriteLine("Left click-rotate counter clockwise");
-            Console.WriteLine("Right click-rotate clockwise");
-            Console.WriteLine("Scroll up-zoom in");
-            Console.WriteLine("Scroll down-zoom out");
-        }
+        private bool _firstMove = true;
 
-        void Keyboard_KeyDown(object sender, KeyboardKeyEventArgs e)
-        {
-            if (e.Key == Key.Escape)
-                this.Exit();
+        private Vector2 _lastPos;
 
-            if (e.Key == Key.F11)
-                if (this.WindowState == WindowState.Fullscreen)
-                    this.WindowState = WindowState.Normal;
-                else
-                    this.WindowState = WindowState.Fullscreen;
 
-            if (e.Key == Key.W)
-                GL.Translate(0.0, 0.1, 0);
+        private bool newStatus = false;
 
-            if (e.Key == Key.S)
-                GL.Translate(0.0, -0.1, 0);
-
-            if (e.Key == Key.D)
-                GL.Translate(0.1, 0, 0);
-
-            if (e.Key == Key.A)
-                GL.Translate(-0.1, 0, 0);
-
-        }
-
-        private void DrawRhombus()
-        {
-            GL.Begin(PrimitiveType.Polygon);
-
-            GL.Color3(Color.Linen);
-            GL.Vertex2(0.0f, 1.0f);
-            GL.Color3(Color.Magenta);
-            GL.Vertex2(-1.0f, 0.0f);
-            GL.Color3(Color.MediumBlue);
-            GL.Vertex2(0.0f, -1.0f);
-            GL.Color3(Color.MediumPurple);
-            GL.Vertex2(1.0f, 0.0f);
-
-            GL.End();
-        }
+        private Window3D() : base(1050, 1050, new GraphicsMode(32, 24, 0, 8)) { }
 
         protected override void OnLoad(EventArgs e)
         {
+            base.OnLoad(e);
+
             GL.ClearColor(Color.MidnightBlue);
+            GL.Enable(EnableCap.DepthTest);
+            GL.Hint(HintTarget.PolygonSmoothHint, HintMode.Nicest);
+
+            _camera = new Camera(new Vector3(0, 0, 20), 1);
+            CursorVisible = false;
+            CursorGrabbed = true;
+
         }
 
         protected override void OnResize(EventArgs e)
         {
+            base.OnResize(e);
+
             GL.Viewport(0, 0, Width, Height);
 
+            double aspect_ratio = Width / (double)Height;
+
+            GL.Translate(new Vector3(0, 0, 2.5f));
+            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)aspect_ratio, 1, 64);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspective);
+
+            Matrix4 lookat = Matrix4.LookAt(posEye.X, posEye.Y, posEye.Z, 0, 0, 0, 0, 1, 0);
             GL.MatrixMode(MatrixMode.Modelview);
-            GL.LoadIdentity();
-            GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
+            GL.LoadMatrix(ref lookat);
+            GL.Translate(new Vector3(0, 0, 2.5f));
+
+            showCube = true;
         }
 
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
-
             base.OnUpdateFrame(e);
 
+            KeyboardState keyboard = Keyboard.GetState();
+            MouseState mouse = Mouse.GetState();
+            var input = Keyboard.GetState();
 
-            MouseState mouse = OpenTK.Input.Mouse.GetState();
 
-            if (mouse[OpenTK.Input.MouseButton.Right])
-                GL.Rotate(-3.0f, 0.0f, 0.0f, 1.0f);
+            // Se utilizeaza mecanismul de control input oferit de OpenTK (include perifcerice multiple, inclusiv
+            // pentru gaminig - gamepads, joysticks, etc.).
+            if (keyboard[Key.Escape])
+            {
+                Exit();
+                return;
+            }
 
-            if (mouse[OpenTK.Input.MouseButton.Left])
-                GL.Rotate(3.0f, 0.0f, 0.0f, 1.0f);
+            if (keyboard[Key.P] && !keyboard.Equals(lastKeyPress))
+            {
+                // Ascundere comandată, prin apăsarea unei taste - cu verificare de remanență! Timpul de reacție
+                // uman << calculator.
+                if (showCube)
+                {
+                    showCube = false;
+                }
+                else
+                {
+                    showCube = true;
+                }
+            }
+            if (keyboard[Key.R] && !keyboard.Equals(lastKeyPress))
+            {
+                if (newStatus)
+                {
+                    newStatus = false;
+                }
+                else
+                {
+                    newStatus = true;
+                }
+            }
 
-            if (mouse.Wheel == +1)
-                GL.Scale(1.1, 1.1, 1.1);
+            if (keyboard[Key.Enter] && keyboard[Key.C])
+            {
+                cube.colors.Clear();
+                cube.SetColors();
+            }
 
-            if (mouse.Wheel == -1)
-                GL.Scale(0.9, 0.9, 0.9);
+            if (keyboard[Key.J])
+            {
+                cube.transparency(-1);
+            }
+
+            if (keyboard[Key.K])
+            {
+                cube.transparency(1);
+            }
+
+
+
+            lastKeyPress = keyboard;
+            const float cameraSpeed = 10.5f;
+            const float sensitivity = 0.2f;
+            if (input.IsKeyDown(Key.W))
+            {
+                _camera.Position += _camera.Front * cameraSpeed * (float)e.Time; // Forward
+            }
+
+            if (input.IsKeyDown(Key.S))
+            {
+                _camera.Position -= _camera.Front * cameraSpeed * (float)e.Time; // Backwards
+            }
+            if (input.IsKeyDown(Key.A))
+            {
+                _camera.Position -= _camera.Right * cameraSpeed * (float)e.Time; // Left
+            }
+            if (input.IsKeyDown(Key.D))
+            {
+                _camera.Position += _camera.Right * cameraSpeed * (float)e.Time; // Right
+            }
+            if (input.IsKeyDown(Key.Space))
+            {
+                _camera.Position += _camera.Up * cameraSpeed * (float)e.Time; // Up
+            }
+            if (input.IsKeyDown(Key.ShiftLeft))
+            {
+                _camera.Position -= _camera.Up * cameraSpeed * (float)e.Time; // Down
+            }
+
+            if (_firstMove) // This bool variable is initially set to true.
+            {
+                _lastPos = new Vector2(mouse.X, mouse.Y);
+                _firstMove = false;
+            }
+            else
+            {
+                // Calculate the offset of the mouse position
+                var deltaX = mouse.X - _lastPos.X;
+                var deltaY = mouse.Y - _lastPos.Y;
+                _lastPos = new Vector2(mouse.X, mouse.Y);
+
+                // Apply the camera pitch and yaw (we clamp the pitch in the camera class)
+                _camera.Yaw += deltaX * sensitivity;
+                _camera.Pitch -= deltaY * sensitivity; // Reversed since y-coordinates range from bottom to top
+            }
         }
 
         protected override void OnRenderFrame(FrameEventArgs e)
         {
-
             base.OnRenderFrame(e);
 
-            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
-            DrawRhombus();
+            if (newStatus)
+            {
+                cube.DrawCube();
+            }
 
-            this.SwapBuffers();
+            if (showCube == true)
+            {
+                GL.PushMatrix();
+                GL.Translate(transStep, attStep, radStep);
+
+                cube.DrawCube();
+                GL.PopMatrix();
+            }
+
+            var value = _camera.GetViewMatrix();
+            GL.LoadMatrix(ref value);
+
+
+            SwapBuffers();
         }
+
 
         [STAThread]
         static void Main(string[] args)
         {
 
-            using (SimpleWindow example = new SimpleWindow())
+            using (Window3D example = new Window3D())
             {
                 example.Run(30.0, 0.0);
             }
+
         }
     }
 }
